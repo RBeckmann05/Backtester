db70394789de978ffbb8178ee86bc395a17b7c9b
Ryan Beckmann <ryan+trading@piels.com>
2025-04-15 15:25:17 -0500

    Update Randomizer.py
    Added volatility setting to randomizing data

--- Randomizer.py
+++ Randomizer.py
@@ -3,7 +3,7 @@ import random
 #data = {0 : [10, 11, 9, 10, 1000], # Bar number : open, high, low, close, volume
 #        1 : [10, 12, 8, 10, 500]} 
 
-def GetRandomData(barsCount=1000, volumeLow=300, volumeHigh=1500, ogOpen=10, ogClose=10, ogVolume=1000):
+def GetRandomData(barsCount=1000, volatility=2, volumeLow=300, volumeHigh=1500, ogOpen=10, ogClose=10, ogVolume=1000):
     data = {}
 
     openPrice = ogOpen
@@ -13,8 +13,8 @@ def GetRandomData(barsCount=1000, volumeLow=300, volumeHigh=1500, ogOpen=10, ogC
     for i in range(barsCount):
         # Simulate a small change
         openPrice = closePrice
-        high = openPrice + random.uniform(0, 2)
-        low = openPrice - random.uniform(0, 2)
+        high = openPrice + random.uniform(0, volatility)
+        low = openPrice - random.uniform(0, volatility)
         closePrice = random.uniform(low, high)
         volume = random.randint(volumeLow, volumeHigh)
 
