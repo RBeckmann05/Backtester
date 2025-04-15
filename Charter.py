fd0389cb64589c86a02338076f7c8bf789756a87
Ryan Beckmann <ryan+trading@piels.com>
2025-04-15 14:26:56 -0500

    Update Charter.py
    Added option to print bar outputs

--- Charter.py
+++ Charter.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
 
-def ChartData(data):
+def ChartData(data, printProgress=False):
     for key, value in data.items():
-        print(key)
+        if printProgress:
+            print(f"Charted Bar: {key}")
         plt.vlines(x=key, ymin=value[2], ymax=value[1], colors='black', lw=1)
         if value[3] > value[0]:
             plt.vlines(x=key, ymin=value[0], ymax=value[3], colors='green', lw=2)
