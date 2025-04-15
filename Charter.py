30e57768121aee3d19947221d68b619130d2c03f
Ryan Beckmann <ryan+trading@piels.com>
2025-04-15 15:25:47 -0500

    Update Charter.py
    Added option to plot indicator data on chart

--- Charter.py
+++ Charter.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 
-def ChartData(data, printProgress=False):
+def ChartData(data, printProgress=False, indicatorData={}):
+    fig, ax = plt.subplots()
     for key, value in data.items():
         if printProgress:
             print(f"Charted Bar: {key}")
@@ -9,4 +10,12 @@ def ChartData(data, printProgress=False):
             plt.vlines(x=key, ymin=value[0], ymax=value[3], colors='green', lw=2)
         else:
             plt.vlines(x=key, ymin=value[3], ymax=value[0], colors='red', lw=2)
+    if len(indicatorData) > 0:
+        xList = []
+        yList = []
+        for i in range(len(indicatorData)):
+            xList.append(i)
+        for value in indicatorData.values():
+            yList.append(value)
+        ax.plot(xList, yList)
     plt.show()
