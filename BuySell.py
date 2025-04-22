46d2aa94520b02033fdcb8babb75a647b3a25de1
Ryan Beckmann <ryan+trading@piels.com>
2025-04-21 19:06:09 -0500

    Update BuySell.py

--- BuySell.py
+++ BuySell.py
@@ -1,25 +1,38 @@
 class OrderStatus:
     def __init__(self, data):
         self.data = data
+        self.marketPosition = 0
+        self.buyPrice = 0
+        self.sellPrice = 0
+        self.netPnl = 0
+        self.netPnlList = [0]
 
-    def Buy(self, orderType="market", orderPrice=0):
+    def Buy(self, orderType, orderPrice):
         if orderType.lower().strip() == "market":
-            buyPrice = self.data[len(self.data)-1][3]
+            buyPrice = orderPrice
+            self.marketPosition += 1
         elif orderType.lower().strip() == "limit":
             if orderPrice > self.data[len(self.data)-1][3]:
                 return "Order failed. Limit buy price must be below current close."
             else:
                 buyPrice = orderPrice
+        self.buyPrice = buyPrice
+        if self.marketPosition == 0:
+            self.CalculatePnl(self.buyPrice, self.sellPrice)
         return buyPrice
     
-    def Sell(self, orderType="market", orderPrice=0):
+    def Sell(self, orderType, orderPrice):
         if orderType.lower().strip() == "market":
-            sellPrice = self.data[len(self.data)-1][3]
+            sellPrice = orderPrice
+            self.marketPosition -= 1
         elif orderType.lower().strip() == "limit":
             if orderPrice < self.data[len(self.data)-1][3]:
                 return "Order failed. Limit sell price must be above current close."
             else:
                 sellPrice = orderPrice
+        self.sellPrice = sellPrice
+        if self.marketPosition == 0:
+            self.CalculatePnl(self.buyPrice, self.sellPrice)
         return sellPrice
     
     def CalculateUnrealized(self, buyPrice, currentPrice, buyOrderFilled=False):
@@ -30,6 +43,8 @@ class OrderStatus:
             return 0
     
     def CalculatePnl(self, buyPrice, sellPrice):
+        self.netPnl += sellPrice-buyPrice
+        self.netPnlList.append(self.netPnl)
         return sellPrice-buyPrice
         
     def SubmitOrder(self, status, orderType, speed, price):
